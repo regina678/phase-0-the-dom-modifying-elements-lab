2fdd0fb91f7f8bae2289d93d0ad2e5b1cbf861e6
Regina Wanjiru Kariuki <wanjiruregina@gmail.com>
2025-02-28 20:53:04 +0300

    creating and inserting DOM nodes

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+// Remove the <main> element
+document.querySelector("main#main").remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Set the id to "victory"
+newHeader.id = "victory";
+
+// Set the text content
+newHeader.textContent = " Wanjiru Kariuki is the champion";
+
+// Append the new header to the body
+document.body.appendChild(newHeader);
